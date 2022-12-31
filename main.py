403c2a55ea68c984fec1c64d123bd45e273d5ecd
Jack Mickel <121540283+jtmic@users.noreply.github.com>
2022-12-31 01:15:58 +0000

    Updated the coding by adding f strings.

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
-
 
 
 bmi = int(weight) / float(height) ** 2
 bmi_as_float = float(bmi)
 bmi_as_int = int(bmi)
 
-if bmi_as_float < 18.5: print("you are underweight.")
+if bmi_as_float < 18.5: 
+    print(f"your bmi is {bmi_as_float}, you are underweight.")
 
-elif bmi_as_int < 25 > 18.5: print("you have normal weight.") 
+elif bmi_as_int < 25: 
+    print(f"your bmi is {bmi_as_float}, you have a normal weight.")
 
-elif bmi_as_int > 25 > 35: print("you are slightly overweight")
+elif bmi_as_int < 30: 
+    print(f"your bmi is {bmi_as_float} you are slightly overweight")
 
-elif bmi_as_int > 30 <+ 35: print("they are obese")
+elif bmi_as_int  < 35: 
+    print(f"your bmi is {bmi_as_float} you are obese")
 
-elif bmi_as_int > 35: print("you are clinically obese")
+elif bmi_as_int > 35: 
+    print(f"your bmi is {bmi_as_float} you are clinically obese")
     
 else: print("you are underweight.")
 
